704c063748ef048780d4cf438508ada0efbd3ea8
maonaoda <maonaoda@gmail.com>
2024-11-13 22:34:32 +0800

    init

---
+++ TestMauiApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TestMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Microsoft.Maui.Controls.NavigationPage.SetHasNavigationBar(this, false);
+	}
+}
+
